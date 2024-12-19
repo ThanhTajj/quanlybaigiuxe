7ff79276bc60143601c4226066b786c04512d13f
ThanhTajj <121273354+ThanhTajj@users.noreply.github.com>
2024-12-19 08:26:23 +0000

    add model and database

--- webw/database/seeders/DatabaseSeeder.php
+++ webw/database/seeders/DatabaseSeeder.php
@@ -19,5 +19,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Book::create([
+            'BookID'=>'b11',
+            'Title'=>'test1',
+            
+        ]);
     }
 }
